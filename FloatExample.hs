c60362d4ddba38b3a654da8be2d7d268a39905be
Timothy Williams <7538854+tim-br@users.noreply.github.com>
2024-05-07 12:36:59 -0700

    use templates.

--- FloatExample.hs
+++ FloatExample.hs
@@ -10,7 +10,7 @@ module FloatExample (
 import Control.Concurrent.MVar
 import qualified Network.WebSockets as WS
 import HViewWebSockets (SendMessage(..), sendJsonMessage, Message(..))
-import Text.Mustache ( compileMustacheText, renderMustache )
+import Text.Mustache ( compileMustacheText, renderMustache, compileMustacheFile )
 import Text.Mustache.Compile
 import GHC.Generics (Generic)
 import Data.Text (Text)
@@ -51,91 +51,51 @@ generateListItem Item {name = name, price = price} = do
 
 createList :: String -> [Item] -> IO String
 createList hid items = do
-  let templateStr = "<h1>Items</h1>\n" ++
-                    "<ul h-id={{hid}}>\n" ++
-                    "{{#items}}\n" ++
-                    "<li>{{{.}}}</li>\n" ++
-                    "{{/items}}\n" ++
-                    "</ul>\n"
-      compiledTemplate = compileMustacheText "mainDiv" (T.pack templateStr)
-  case compiledTemplate of
-    Left _ -> return "Error compiling main div template"
-    Right template -> do
-      itemStrings <- mapM generateListItem items
-      let rendered = renderMustache template $ object ["items" .= itemStrings, "hid" .= hid]
-      return $ TL.unpack rendered
+  template <- compileMustacheFile "templates/list.mustache"
+  itemStrings <- mapM generateListItem items
+  let rendered = renderMustache template $ object ["items" .= itemStrings, "hid" .= hid]
+  return $ TL.unpack rendered
 
-createMainDiv :: String -> String -> IO String
-createMainDiv value id = do
-  let templateStr = "<div h-id={{id}} target-id=\"bottomDiv\">{{{content}}}</div>"
-      compiledTemplate = compileMustacheText "mainDiv" templateStr
-  case compiledTemplate of
-    Left bundle -> return "Error compiling main div template"
-    Right template -> do
+createMainDiv :: String -> IO String
+createMainDiv id = do
+      template <- compileMustacheFile "templates/mainDiv.mustache"
       let rendered = renderMustache template $ object
             [ "id"      .= id
-            , "content" .= TL.pack value  -- Convert String to Text
             ]
       return $ TL.unpack rendered  -- Convert Text back to String
 
 counterDiv :: String -> String -> IO String
 counterDiv hId value = do
-  let templateStr = "<div h-id={{id}} target-id={{id}}><h1>Counter: <div h-value>{{count}}</div></h1><button h-click=\"increment\">Increment</button><button h-click=\"decrement\">Decrement</button></div>"
-      compiledTemplate = compileMustacheText "mainDiv" templateStr
-  case compiledTemplate of
-    Left _ -> return "Error compiling main div template"
-    Right template -> do
-      let rendered = renderMustache template $ object
-            [ "id"    .= hId
-            , "count" .= TL.pack value  -- Convert String to Text
-            ]
-      return $ TL.unpack rendered
+  template <- compileMustacheFile "templates/counterDiv.mustache"
+  let rendered = renderMustache template $ object
+        [ "id"    .= hId
+        , "count" .= TL.pack value  -- Convert String to Text
+        ]
+  return $ TL.unpack rendered
 
 -- Function to create the bottom div using Mustache
 createBottomDiv :: String -> IO String
 createBottomDiv valueClass = do
-  let templateStr = "<div h-id=\"bottomDiv\" class=\"{{valueClass}}\"></div>"
-      compiledTemplate = compileMustacheText "bottomDiv" templateStr
-  case compiledTemplate of
-    Left bundle -> return "Error compiling bottom div template"
-    Right template -> do
-      let rendered = renderMustache template $ object
-            [ "valueClass"      .= valueClass
-            ]
-      return $ TL.unpack rendered  -- Convert Text back to String
-
-
--- instance Renderer Instance2 where
---     render hid (Instance2 value ) = do
---       let templateStr = "<div h-id={{id}} target-id=\"bottomDiv\"><div h-value>{{value}}</div> <button h-click=\"set-float\">Show Float</button><div h-id\"bottomDiv\" class=\"{{value}}\"></div></div>"
---       let compiledTemplate = compileMustacheText "page" templateStr
---       case compiledTemplate of
---         Left bundle ->  "Error compiling template"
---         Right template -> renderMustache template $ object
---           [ "value" .= (value :: String)
---           , "id"    .= (hid :: String) ]
+  template <- compileMustacheFile "templates/bottomDiv.mustache"
+  let rendered = renderMustache template $ object
+        [ "valueClass"      .= valueClass
+        ]
+  return $ TL.unpack rendered  -- Convert Text back to String
 
 
 render :: String -> String -> IO String
 render hid value = do
-  let buttonStr = "<button h-click=\"set-float\">Show Float</button>"
-  let unsetButtonStr = "<button h-click=\"unset-float\">Hide Float</button>"
-  let newValue = buttonStr ++ unsetButtonStr
-  mainDivContent <- createMainDiv newValue hid
+  mainDivContent <- createMainDiv hid
   list <- createList "list" items
   bottomDivContent <- createBottomDiv value
   counterDiv <- counterDiv "fixx" "0"
   let templateStr = "{{{mainDiv}}}{{{counterDiv}}} {{{list}}} {{{bottomDiv}}}"
-      -- buttonStr = "<button h-click=\"set-float\">Show Float</button>"
-      -- unsetButtonStr = "<button h-click=\"unset-float\">Hide Float</button>"
       compiledTemplate = compileMustacheText "page" templateStr
   case compiledTemplate of
     Left _ -> return "Error compiling page template"
     Right template -> do
       let rendered = renderMustache template $ object
             [ "mainDiv"   .= T.pack mainDivContent  -- Convert String to Text
-            , "button"    .= T.pack buttonStr  -- Convert String to Text
-            , "unsetButton"    .= T.pack unsetButtonStr  -- Convert String to Text
             , "counterDiv" .= T.pack counterDiv
             , "bottomDiv" .= T.pack bottomDivContent  -- Convert String to Text
             , "list" .= T.pack list
